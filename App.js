8f0eb5ae76335c7de4e71ff7d23b1cb1498292bd
maureen-tellez <maureentellez0816@gmail.com>
2025-11-03 18:55:22 -0600

    prinera parte

--- App.js
+++ App.js
@@ -1,13 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect, use } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity,Alert } from 'react-native';
+import MapView, {Maker} from 'react-native-maps';
+import * as Location from 'expo-location';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  const [location,setLocation] = useState(null)
+  const [loading,setLoading]= useState(null)
+  const [error,setError]=useState(null)
+
+  useEffect(()=>{
+    getLocation()
+  }, [])
+
+  async function getLocation(){
+    try{
+      setLoading(true)
+      setError(null)
+
+      let {status} = await Location.requestBackgroundPermissionsAsync()
+      if(status !== 'granted'){
+        setError('Permiso de ubicación negado')
+        setLoading(false)
+        return
+      }
+    } catch(error){
+      
+    }
+  } 
+
+
+  return(
+    <>
+    </>
   );
+
 }
 
 const styles = StyleSheet.create({
